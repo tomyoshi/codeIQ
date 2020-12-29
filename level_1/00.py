99b46080bfa285a72ebcb5307d78eee540b42222
ty444 <tomyoshi1995@gmail.com>
2020-12-29 21:51:01 +0900

    add level_1 dir and Q1's ans

---
+++ level_1/00.py
@@ -0,0 +1,19 @@
+
+
+def main():
+	num = 11
+	
+	while True:
+		if str(num) == str(num)[::-1] and \
+		bin(num)[2::] == bin(num)[:1:-1] and \
+		oct(num)[2::] == oct(num)[:1:-1]:
+			print(num)
+			break
+		else:
+			num = num + 1
+
+
+if __name__ == "__main__":
+	main()
+
+
