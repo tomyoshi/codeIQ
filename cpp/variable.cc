ff73e9ccf055c0e4e739161b0d9792aab26b7642
ty444 <tomyoshi1995@gmail.com>
2021-02-03 21:24:20 +0900

    this is a cppcode

---
+++ cpp/variable.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main() {
+	int x = 5;
+	int y;
+	y = 5;
+
+	const std::string cstr = "aaaa";
+
+	std::cout << x << std::endl;
+	std::cout << y << std::endl;
+	std::cout << cstr << std::endl;
+}
